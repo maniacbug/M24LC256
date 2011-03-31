9f40274033fd6e35963a0ef0781262baee93e443
maniacbug <maniacbug@ymail.com>
2011-03-30 19:45:57 -0700

    Added

---
+++ M24LC256.cpp
@@ -0,0 +1,156 @@
+/*
+ Copyright (C) 2011 James Coliz, Jr. <maniacbug@ymail.com>
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+ */
+
+#include <Wire.h>
+#include "M24LC256.h"
+
+// M24LC256 base address on I2C bus
+
+const uint8_t I2C_M24LC256 = B1010000;
+
+M24LC256::M24LC256(uint8_t _address)
+{
+    i2c_address = _address | I2C_M24LC256;
+}
+
+// Call AFTER "Wire.begin()"
+void M24LC256::begin(void)
+{
+    // Actually, we have nothing to do
+}
+
+// Note that we use an internal "page size" of 16.  The chip has a page size of 64 bytes,
+// and the wire library can only send 30 at a time.  So the largest even factor of 64 that's
+// less than 30 is 16, so we write & read even chunks of 16.
+//
+// The public read/write functions can read/write any amount of data, to any location.
+// Pages are automaticaly handled so we never do a page write over a page boundary.
+
+// A chunk is the largest even factor of a page size that the Wire library can write,
+// 16 bytes
+
+// internal page size scales to the size of the Wire buffer.  Increase these values in Wire.h and utility/twi.h
+// to get much faster speed.  The chip has a 5msec write cycle no matter how many bytes you push it, up to
+// 64.
+
+#if ( BUFFER_LENGTH < 34 ) || ( TWI_BUFFER_LENGTH < 34 )
+const int page_size = 0x10;
+#elif ( BUFFER_LENGTH < 66 ) || ( TWI_BUFFER_LENGTH < 66 )
+const int page_size = 0x20;
+#else
+const int page_size = 0x40;
+#endif
+
+uint16_t M24LC256::WriteChunk(uint16_t location, uint16_t length, uint8_t* data)
+{
+    uint16_t bytes_written = min(length,page_size);
+
+    //printf("Writing %u to %u from %p\n",bytes_written,location,data);
+
+    Wire.beginTransmission(i2c_address);  // START condition
+    Wire.send((uint8_t)(location >> 8));
+    Wire.send((uint8_t)(location & 0xff));
+    Wire.send(data,bytes_written);
+    Wire.endTransmission();
+    delay(5);  // 5msec write cycle time (Datasheet, pg.4
+
+    return bytes_written;
+}
+
+void M24LC256::write(uint16_t location, void* buf, uint16_t len)
+{
+    uint16_t remaining = len;
+    uint8_t* next_in = reinterpret_cast<uint8_t*>(buf);
+    uint16_t next_out = location;
+
+    // First, deal with page boundaries.  If location is NOT on an even 16 bytes,
+    // we'll want to write out only enough to get to the end of this page.
+
+    if ( location & (page_size-1) )
+    {
+        uint16_t chunk_length = min(remaining, page_size - ( location & (page_size-1) ));
+
+        uint16_t written = WriteChunk( next_out, chunk_length, next_in );
+        remaining -= written;
+        next_in += written;
+        next_out += written;
+    }
+
+    while ( remaining )
+    {
+        uint16_t written = WriteChunk( next_out, remaining, next_in );
+        remaining -= written;
+        next_in += written;
+        next_out += written;
+    }
+}
+
+uint16_t M24LC256::ReadChunk(uint16_t location, uint16_t length, uint8_t* data)
+{
+    uint16_t bytes_received = 0;
+    uint16_t bytes_requested = min(length,16);
+
+    Wire.beginTransmission(i2c_address);  // START condition
+    Wire.send((uint8_t)(location >> 8));
+    Wire.send((uint8_t)(location & 0xff));
+
+    Wire.endTransmission();
+
+    Wire.requestFrom(i2c_address,bytes_requested);
+    uint16_t remaining = length;
+    uint8_t* next = data;
+
+    while (Wire.available() && remaining--)
+    {
+        *next++ = Wire.receive();
+        ++bytes_received;
+    }
+
+    *next = 0; // null terminate fur debug printing purposes
+    //printf("Read %u from %u to %p %s\n",bytes_received,location,data, (char*)data);
+
+    delay(1);
+    return bytes_received;
+}
+
+uint16_t M24LC256::read(uint16_t location, void* buf, uint16_t len)
+{
+    uint16_t bytes_received = 0;
+    uint16_t remaining = len;
+    uint8_t* next_memory = reinterpret_cast<uint8_t*>(buf);
+    uint16_t next_chip = location;
+
+    // First, deal with page boundaries.  If location is NOT on an even 16 bytes,
+    // we'll want to write out only enough to get to the end of this page.
+
+    if ( location & 0xf )
+    {
+        uint16_t chunk_length = min( remaining, 0x10 - ( location & 0xf ) );
+
+        uint16_t chunk_received = ReadChunk(next_chip,chunk_length,next_memory);
+        remaining -= chunk_received;
+        next_memory += chunk_received;
+        next_chip += chunk_received;
+        bytes_received += chunk_received;
+    }
+
+    while ( remaining )
+    {
+        uint16_t chunk_received = ReadChunk(next_chip,remaining,next_memory);
+        remaining -= chunk_received;
+        next_memory += chunk_received;
+        next_chip += chunk_received;
+        bytes_received += chunk_received;
+        if ( ! chunk_received ) // if we don't get any data, fall out so we don't hang here forever.
+            break;
+    }
+
+    return bytes_received;
+}
+
+// vim:ci:sw=4 sts=4 ft=cpp
---
+++ M24LC256.h
@@ -0,0 +1,133 @@
+/*
+ Copyright (C) 2011 James Coliz, Jr. <maniacbug@ymail.com>
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+ */
+
+#include <WProgram.h>
+
+/**
+ * Driver for Microchip 24LC256 EEPROM
+ *
+ * See <a href="http://ww1.microchip.com/downloads/en/DeviceDoc/21203Q.pdf">datasheet</a>
+ *
+ * Create one instance of this class for each chip in your circuit.  This allows you
+ * to have up to 8 of these active at any time.
+ *
+ * This class is
+ * optimized to take advantage of the page size of the chip.  The chip has a 5msec
+ * write cycle now matter how many bytes you push it, up to its page size (64 bytes
+ * for 24LC256.  To get higher throughput, increase the size of the Wire buffer by
+ * increasing BUFFER_LENGTH in Wire.h and TWI_BUFFER_LENGTH in utility/twi.h.  Increase
+ * them to 34 for 32-byte writes, and to 66 for 64-byte writes.  Be warned that these
+ * buffers suck up your memory, so be careful!
+ */
+
+class M24LC256
+{
+protected:
+    /**
+     * The 7-bit address of the particular device we're driving on the I2C bus.
+     */
+    uint8_t i2c_address;
+
+    /**
+     * @name Internals
+     *
+     * Protected methods that address the chip directly.
+     */
+    /**@{*/
+    /**
+     * Writes a small chunk of data directly to the chip.
+     *
+     * Only 16 bytes maximum are written at a time.  Less bytes are written
+     * if @p location is not on an even 16-byte boundary.
+     *
+     * @param location The chip address where to write the data
+     * @param length The number of bytes available to be written
+     * @param data The in-memory location to find the data
+     *
+     * @returns The number of bytes written to the chip
+     */
+    uint16_t WriteChunk(uint16_t location, uint16_t length, uint8_t* data);
+
+    /**
+     * Reads a small chunk of data directly from the chip.
+     *
+     * Only 16 bytes maximum are read at a time.  Less bytes are read
+     * if @p location is not on an even 16-byte boundary.
+     *
+     * @param location The chip address where to read the data
+     * @param length The maximum number of bytes desired to be read
+     * @param data The in-memory location of where to place the data
+     *
+     * @returns The number of bytes read from the chip
+     */
+    uint16_t ReadChunk(uint16_t location, uint16_t length, uint8_t* data);
+
+    /**@}*/
+public:
+    /**
+     * Constructor
+     *
+     * @param _address the low 3 bits of the address for the chip being
+     * addressed.  This should correspond to the A0-A2 pins.
+     */
+    M24LC256(uint8_t _address);
+
+    /**
+     * Setup & prepare
+     *
+     * Call this function from within the setup() function of your
+     * sketch, AFTER Wire.begin().
+     *
+     * Note that this function currently does nothing, but is included for forward
+     * compatability.  Someday something may be needed here.
+     */
+    void begin(void);
+
+    /**
+     * Write data to any location on the chip
+     *
+     * This function is not constrained by the size or location of data.  Write what
+     * you want, where you want.
+     *
+     * @warning This will not write beyond the top of the address space of the chip.
+     * (0x8000).  If you attempt to write beyond the upper limit, only the amount which
+     * fits within the memory limit is written, the rest is ignored.
+     *
+     * @param location The chip address where to write the data
+     * @param buf The in-memory location of where to look for the data
+     * @param len The number of bytes to be written
+     */
+    void write(uint16_t location, void* buf, uint16_t len);
+
+    /**
+     * Read data from any location on the chip
+     *
+     * This function is not constrained by the size or location of data.  Write what
+     * you want, where you want.
+     *
+     * @warning This will not read beyond the top of the address space of the chip.
+     * (0x8000).  If you attempt to read beyond the upper limit, only the amount found
+     * before the memory limit is read. Check the return value for actual number of
+     * bytes read.
+     *
+     * @param location The chip address where to read the data
+     * @param buf The in-memory location of where to place the data
+     * @param len The maximum number of bytes desired to be read
+     *
+     * @returns The number of bytes read from the chip
+     */
+
+    uint16_t read(uint16_t location, void* buf, uint16_t len);
+};
+
+/** @mainpage Driver library for 24LC256 EEPROM
+ *
+ * See the M24LC256 class for details on how to drive this chip.
+ */
+
+// vim:ci:sw=4 sts=4 ft=cpp
